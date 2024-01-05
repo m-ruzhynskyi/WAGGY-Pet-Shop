9df3849be7806233b14466ef2c2d4faa936e47a8
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2024-01-05 17:23:43 +0200

    add html and scss, make a min, will do more

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat')
+const scss = require('gulp-sass')(require('sass'))
+const uglify = require('gulp-uglify-es').default;
+const clean = require('gulp-clean');
+const htmlMin = require('gulp-htmlmin')
+
+gulp.task('minScripts', () => {
+    return gulp.src([
+        './node_modules/@splidejs/splide/dist/js/splide.js',
+        './js/petFoodies.js',
+        './js/petFood.js',
+        './js/main.js',
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js/'))
+})
+gulp.task('minScss', () => {
+    return gulp.src('./css/main.scss')
+        .pipe(concat('main.min.css'))
+        .pipe(scss({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./css'))
+})
+gulp.task('minHTML', () =>{
+    return gulp.src('./index.html')
+        .pipe(htmlMin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'))
+})
+gulp.task('cleanDist', () => {
+    return gulp.src('dist')
+        .pipe(clean())
+})
+
