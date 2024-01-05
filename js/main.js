01503cbb22e15668d5bdca2c4196b4dcdfac759a
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2024-01-05 20:37:50 +0200

    add all blocks for mainBlock, styles, make a logic for bar with animal food

--- js/main.js
+++ js/main.js
@@ -1,7 +1,49 @@
-let splide = new Splide( '#headSplider', {
+let headSplider = new Splide( '#headSplider', {
     type  : 'loop',
     rewind: true,
     speed: 800,
 } );
 
-splide.mount();
+headSplider.mount()
+
+let productCarousel = new Splide( '.products__carousel', {
+    type  : 'slide',
+    perPage: 4,
+    rewind: true,
+    speed: 800,
+    pagination: false
+} );
+
+productCarousel.mount();
+
+let bestSelling = new Splide( '.bestSelling__carousel', {
+    type  : 'slide',
+    perPage: 4,
+    rewind: true,
+    speed: 800,
+    pagination: false
+});
+
+bestSelling.mount();
+
+let reviewsCarousel = new Splide( '.reviews__carousel', {
+    type  : 'loop',
+    rewind: true,
+    speed: 800,
+    pagination: true,
+    arrows: false
+} );
+
+reviewsCarousel.mount();
+window.addEventListener('DOMContentLoaded', () => {
+    new PetFood().init()
+})
+document.querySelectorAll('.categories__title').forEach(category => {
+    category.addEventListener('click', (e) =>{
+        document.querySelectorAll('.categories__title').forEach(category => {
+            category.style.borderBottom = '2px solid #D9D9D8'
+        })
+        e.target.style.borderBottom = '2px solid #DEAD6F'
+        new ShowCategory(e.target.textContent.toLowerCase())
+    })
+})
---
+++ js/petFoodies.js
@@ -0,0 +1,76 @@
+const petFoodies = {
+    cat:{
+        C001:{
+            imgBlock:{
+                img: './assets/img/food4.png',
+            },
+            h3: 'Grey Hoodie',
+            rating: 5,
+            p: 18
+        },
+        C002:{
+            imgBlock:{
+                img: './assets/img/food.png',
+                promo: 'sold',
+            },
+            h3: 'Grey Hoodie',
+            rating: 5,
+            p: 18
+        },
+        C003:{
+            imgBlock:{
+                img: './assets/img/food5.png',
+            },
+            h3: 'Grey Hoodie',
+            rating: 5,
+            p: 18
+        },
+    },
+    dog:{
+        D001:{
+            imgBlock:{
+                img: './assets/img/food2.png',
+                promo: 'new',
+            },
+            h3: 'Grey Hoodie',
+            rating: 5,
+            p: 18
+        },
+        D002:{
+            imgBlock:{
+                img: './assets/img/food3.png',
+            },
+            h3: 'Grey Hoodie',
+            rating: 5,
+            p: 18
+        },
+        D003:{
+            imgBlock:{
+                img: './assets/img/food6.png',
+                promo: 'sold',
+            },
+            h3: 'Grey Hoodie',
+            rating: 5,
+            p: 18
+        },
+    },
+    bird:{
+        B001:{
+            imgBlock:{
+                img: './assets/img/food7.png',
+            },
+            h3: 'Grey Hoodie',
+            rating: 5,
+            p: 18
+        },
+        B002:{
+            imgBlock:{
+                img: './assets/img/food8.png',
+                promo: 'sold',
+            },
+            h3: 'Grey Hoodie',
+            rating: 5,
+            p: 18
+        },
+    },
+}
